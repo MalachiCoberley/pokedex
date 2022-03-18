54e13ad884e65fbdb62ec04185de9df8718c12b0
MalachiCoberley <malachicoberley@gmail.com>
2022-03-17 19:25:40 -0700

    sexond commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {pokemon} from "../stores/stores.js"
+  console.log($pokemon)
+</script>
+
+<svelte:head>
+  <title>Pokedex</title>
+</svelte:head>
+
+<h1>Pokedex Demo</h1>
+
+{#each $pokemon as pokeman}
+<p>{pokeman.name}</p>
+{/each}
